6fa12b4fbbb48a61ad4773aea57a16e1512e959c
Maru625 <gks_wodnr@naver.com>
2025-09-19 00:47:13 +0900

    더미 탭, 0919 테스트 로직 0402 데이터 관리 추가 버전

--- modules/monitoring/logic/replan_actual_logic.py
+++ modules/monitoring/logic/replan_actual_logic.py
@@ -1,9 +1,10 @@
 # logic/replan_actual_logic.py: 실제 재계획 판단 로직을 수행하는 함수를 정의합니다.
+from datetime import datetime, timezone
 
 from logic.Replan.replan_management import ReplanManager
 import udp_reporter
 from data.message_models import ReplanRequestBodyModel
-from datetime import datetime, timezone
+from push.push_center import push_message
 
 
 def run_replan_procedure(manager):
@@ -52,25 +53,25 @@ def run_replan_procedure(manager):
     # 0902 ReplanRequest 메시지 본문 생성
     ## 0918 적 발견으로 인한 유인기 경로 재계획 명령 확인용 더미 데이터
     final_replan_output = "적 탐지로 인한 유인기 공격 판단 필요"
+    if situationAwarenessInfo:
+        timestamp = int(
+            (
+                datetime.now(timezone.utc) - datetime(2000, 1, 1, tzinfo=timezone.utc)
+            ).total_seconds()
+            * 1000
+        )
+        replan_body = ReplanRequestBodyModel(
+            timestamp=timestamp,
+            sourceModuleName="MonitoringModule",
+            replanLevel=3,  # 유인기 공격 모델 호출 / 경로 및 촬영 재계획
+            replanRequest=final_replan_output,  # final_replan_output을 replanRequest 필드에 사용
+        )
 
-    timestamp = int(
-        (
-            datetime.now(timezone.utc) - datetime(2000, 1, 1, tzinfo=timezone.utc)
-        ).total_seconds()
-        * 1000
-    )
-    replan_body = ReplanRequestBodyModel(
-        timestamp=timestamp,
-        sourceModuleName="MonitoringModule",
-        replanLevel=3,  # 유인기 공격 모델 호출 / 경로 및 촬영 재계획
-        replanRequest=final_replan_output,  # final_replan_output을 replanRequest 필드에 사용
-    )
+        push_message("0902", manager.node_messenger, body_dict=replan_body)
+        manager.receive_store.set_data("0402", None)
 
-    # PushStorage에 저장
-    manager.push_store.add_data("0902", replan_body)
+        # PushStorage에 저장
+        manager.push_store.add_data("0902", replan_body)
 
-    # 재계획 결과가 저장되었음을 UDP로 통지
-    udp_reporter.notify_tx("0902")
-
-    manager._log("REPLAN_PROCEDURE", "INFO", f"최종 재계획 결과: {final_replan_output}")
-    manager._log("REPLAN_PROCEDURE", "INFO", "실제 재계획 판단 로직 실행 종료.")
+        # 재계획 결과가 저장되었음을 UDP로 통지
+        udp_reporter.notify_tx("0902")
